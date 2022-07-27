a863c99c1e7773e79508451509dda8e418d2e6ee
gio <giovany.torres.799@gmail.com>
2022-07-26 21:57:49 -0500

    scripts

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
 
 
-function seleccionarMascota(mascota) {
-    alert('seleccionaste tu mascota')   
-    if (document.getElementById("Hipodoge").checked) {
-        console.log('elegiste hypodoge')
-    } else if(document.getElementById('Hipodoge')){
-        console.log('elegiste hypodoge')
+function seleccionarMascota() {
+    let hypodoge = document.getElementById("Hipodoge")
+    let Capipeyo = document.getElementById("Capipeyo")
+    let Ratigueya = document.getElementById("Ratigueya")
+    let Langostelvis = document.getElementById("Langostelvis")
+    let Tupacalma = document.getElementById("Tupacalma")
+    let Pydos = document.getElementById("Pydos")
+    let spanMascotaJugador= document.getElementById('mascota_jugador')
+    if (hypodoge.checked) {
+       spanMascotaJugador.innerHTML= 'hypodoge' 
+    }     else if(Capipeyo.checked){
+        spanMascotaJugador.innerHTML= 'Capipeyo' 
+    }    else if(Ratigueya.checked){
+        spanMascotaJugador.innerHTML= 'Ratigueya' 
+    }    else if(Langostelvis.checked){
+        spanMascotaJugador.innerHTML= 'Langostelvis' 
+    }    else if(Tupacalma.checked){
+        spanMascotaJugador.innerHTML= 'Tupacalma' 
+    }    else if(Langostelvis.checked){
+        spanMascotaJugador.innerHTML= 'Langostelvis' 
+    }    else if(Pydos.checked){
+        spanMascotaJugador.innerHTML= 'Pydos' 
+    }
+    else{
+        alert('no seleccionaste ninguna mascota ')
     }
 }
 
 function iniciarJuego(mascota) {
     let btn_mascota = document.getElementById("btn-pet")
-    let btn_Hipodoge = document.getElementById("Hipodoge")
+    
     let btn_fuego = document.getElementById("btn-fire")
     let btn_agua = document.getElementById("btn-water")
     let btn_tierra= document.getElementById("btn-ground")
