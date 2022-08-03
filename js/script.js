844d1a5e31604b491935ceb5dda1d30fc2751c9c
gio <giovany.torres.799@gmail.com>
2022-08-03 16:24:54 -0500

    OptimizandoFronted

--- js/script.js
+++ js/script.js
@@ -172,14 +172,17 @@ function secuenciaAtaque() {
                 ataqueJugador.push('FUEGO')
                 console.log(ataqueJugador);
                 boton.style.background = '#112f58'
+                boton.disabled=true
             } else if (e.target.textContent === '💧') {
                 ataqueJugador.push('AGUA')
                 console.log(ataqueJugador);
                 boton.style.background = '#112f58'
+                boton.disabled=true
             } else {
                 ataqueJugador.push('TIERRA')
                 console.log(ataqueJugador);
                 boton.style.background = '#112f58'
+                boton.disabled=true
             }
             ataqueAleatorioEnemigo()
         })
@@ -221,8 +224,6 @@ function combate() {
         if (ataqueJugador[index] === ataqueEnemigo[index]) {
             indexAmbosOponente(index, index)
             crearMensaje("EMPATE")
-            victoriasJugador++
-            spanVidasJugador.innerHTML = victoriasJugador
         } else if (ataqueJugador[index] === 'FUEGO' && ataqueEnemigo[index] === 'TIERRA') {
             indexAmbosOponente(index, index)
             crearMensaje("GANASTE")
@@ -267,9 +268,7 @@ function crearMensaje(resultado) {
 }
 function crearMensajeFinal(resultadoFinal) {
     sectionMensajes.innerHTML = resultadoFinal
-    botonFuego.disabled = true
-    botonAgua.disabled = true
-    botonTierra.disabled = true
+
     sectionReiniciar.style.display = 'block'
 }
 function reiniciarJuego() {
